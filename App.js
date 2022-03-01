43fb65c6520f80a31bd66eacad6d59f4a69e2ffd
Nicole Lee <njlee@stanford.edu>
2022-02-28 19:41:31 -0800

    Added h1 tag”

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Screens/home';
+import MyLocation from './Screens/myLocation';
+import FriendsLocation from './Screens/friendsLocation';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator
+        screenOptions={{
+            headerShown: false
+        }}
+        initialRouteName={'Home'}
+    >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="MyLocation" component={MyLocation} />
+        <Stack.Screen name="FriendsLocation" component={FriendsLocation} />
+    </Stack.Navigator>
+</NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F3F2F2',
     alignItems: 'center',
     justifyContent: 'center',
   },
